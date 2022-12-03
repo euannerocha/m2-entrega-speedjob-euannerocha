36c342d9f9d6c63d5de39a4169ad977256b93c64
euannerocha <annelisesr@gmail.com>
2022-12-03 16:50:52 -0300

    CSS dos cards finalizado, backend e frontend

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -37,3 +37,43 @@ function renderCardsFront(array){
 }
 
 renderCardsFront(frontEndJobs)
+
+
+function renderCardsBack(array){
+    let ulBackendJobs = document.querySelector('.ulBackendJobs')
+    ulBackendJobs.innerText = '';
+    for(var i=0; i < array.length; i++){
+        let liFront = document.createElement('li')
+            let divHeaderCardFront = document.createElement('div')
+                let iconCardFront = document.createElement('p')
+                let titleCardFront = document.createElement('h1')
+                let locationCardFront = document.createElement('p')
+            let jobDescriptionCardFront = document.createElement('p')
+            let tagWorkloadCardFront = document.createElement('p')
+            let tagModalityCardFront = document.createElement('p')
+
+    liFront.classList.add('liBack')
+    divHeaderCardFront.classList.add('divHeaderCardBack')
+    iconCardFront.classList.add('iconCardBack')
+    titleCardFront.classList.add('titleCardBack')
+    locationCardFront.classList.add('locationCardBack')
+    jobDescriptionCardFront.classList.add('jobDescriptionCardBack')
+    tagWorkloadCardFront.classList.add('tagWorkloadCardBack')
+    tagModalityCardFront.classList.add('tagModalityCardBack')
+
+    iconCardFront.innerText = 'G'
+    titleCardFront.innerText = array[i].title
+    locationCardFront.innerText = 'Curitiba - Paraná'
+    jobDescriptionCardFront.innerText = array[i].descrription
+    tagWorkloadCardFront.innerText = array[i].modality[0]
+    tagModalityCardFront.innerText = array[i].modality[1]
+    
+
+    ulBackendJobs.append(liFront)
+    liFront.append(divHeaderCardFront, jobDescriptionCardFront, tagWorkloadCardFront, tagModalityCardFront)
+    divHeaderCardFront.append(iconCardFront, titleCardFront, locationCardFront)
+
+ }
+}
+
+renderCardsBack(backEndJobs)
